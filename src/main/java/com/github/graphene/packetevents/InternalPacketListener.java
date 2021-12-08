a9a90649be7b55e47a4d4922832b17f46acbfee0
retrooper <retrooperdev@gmail.com>
2021-12-08 19:02:23 +0100

    Join game packet isn't being sent successfully.

--- src/main/java/com/github/graphene/packetevents/InternalPacketListener.java
+++ src/main/java/com/github/graphene/packetevents/InternalPacketListener.java
@@ -2,6 +2,7 @@ package com.github.graphene.packetevents;
 
 import com.github.graphene.Graphene;
 import com.github.graphene.user.User;
+import com.github.graphene.wrapper.play.server.WrapperPlayServerJoinGame;
 import com.github.graphene.wrapper.play.server.WrapperStatusServerResponse;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketListener;
@@ -9,17 +10,26 @@ import com.github.retrooper.packetevents.event.impl.PacketReceiveEvent;
 import com.github.retrooper.packetevents.event.impl.PacketSendEvent;
 import com.github.retrooper.packetevents.manager.player.attributes.TabCompleteAttribute;
 import com.github.retrooper.packetevents.protocol.ConnectionState;
+import com.github.retrooper.packetevents.protocol.nbt.*;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.wrapper.handshaking.client.WrapperHandshakingClientHandshake;
 import com.github.retrooper.packetevents.wrapper.login.client.WrapperLoginClientLoginStart;
+import com.github.retrooper.packetevents.wrapper.login.server.WrapperLoginServerLoginSuccess;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientSettings;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientTabComplete;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerHeldItemChange;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerPositionAndLook;
 import com.github.retrooper.packetevents.wrapper.status.client.WrapperStatusClientPing;
 import com.github.retrooper.packetevents.wrapper.status.server.WrapperStatusServerPong;
 import com.google.gson.JsonObject;
 import io.netty.channel.Channel;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public class InternalPacketListener implements PacketListener {
     @Override
@@ -67,8 +77,7 @@ public class InternalPacketListener implements PacketListener {
 
                     WrapperStatusServerResponse response = new WrapperStatusServerResponse(responseComponent);
                     PacketEvents.getAPI().getPlayerManager().sendPacket(event.getChannel(), response);
-                }
-                else if (event.getPacketType() == PacketType.Status.Client.PING) {
+                } else if (event.getPacketType() == PacketType.Status.Client.PING) {
                     WrapperStatusClientPing ping = new WrapperStatusClientPing(event);
                     long time = ping.getTime();
 
@@ -76,7 +85,7 @@ public class InternalPacketListener implements PacketListener {
                     PacketEvents.getAPI().getPlayerManager().sendPacket(event.getChannel(), pong);
                     //Close channel
                     //TODO Contribute some better way in packetevents to close the channel
-                    ((Channel)event.getChannel().rawChannel()).close();
+                    ((Channel) event.getChannel().rawChannel()).close();
                 }
                 break;
             case LOGIN:
@@ -85,6 +94,90 @@ public class InternalPacketListener implements PacketListener {
                     //Map the player usernames with their netty channels
                     PacketEvents.getAPI().getPlayerManager().CHANNELS.put(start.getUsername(), event.getChannel());
                     user.setUsername(start.getUsername());
+                    UUID uuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + user.getUsername()).getBytes(StandardCharsets.UTF_8));
+                    user.setUUID(uuid);
+
+                    WrapperLoginServerLoginSuccess loginSuccess = new WrapperLoginServerLoginSuccess(user.getUUID(), user.getUsername());
+                    PacketEvents.getAPI().getPlayerManager().sendPacket(event.getChannel(), loginSuccess);
+                    System.out.println("[Graphene] " + user.getUsername() + " has logged in.");
+                    List<String> worldNames = new ArrayList<>();
+                    worldNames.add("world");
+                    worldNames.add("world2");
+                    worldNames.add("world3");
+
+                    long hashedSeed = 100L;
+                    NBTCompound dimension = new NBTCompound();
+                    dimension.setTag("piglin_safe", new NBTByte((byte) 1));
+                    dimension.setTag("natural", new NBTByte((byte) 1));
+                    dimension.setTag("ambient_light", new NBTFloat(1.0f));
+                    dimension.setTag("infiniburn", new NBTString(""));
+                    dimension.setTag("respawn_anchor_works", new NBTByte((byte) 1));
+                    dimension.setTag("has_skylight", new NBTByte((byte) 1));
+                    dimension.setTag("bed_works", new NBTByte((byte) 1));
+                    dimension.setTag("effects", new NBTString("minecraft:overworld"));
+                    dimension.setTag("has_raids", new NBTByte((byte) 1));
+                    dimension.setTag("min_y", new NBTInt(0));
+                    dimension.setTag("height", new NBTInt(400));
+                    dimension.setTag("logical_height", new NBTInt(256));
+                    dimension.setTag("coordinate_scale", new NBTInt(1));
+                    dimension.setTag("ultrawarm", new NBTByte((byte) 1));
+                    dimension.setTag("has_ceiling", new NBTByte((byte) 1));
+                    NBTCompound dimensionCodec = new NBTCompound();
+                    NBTCompound dimensionType = new NBTCompound();
+                    NBTList<NBTCompound> types = new NBTList<>(NBTType.COMPOUND);
+                    NBTCompound type = new NBTCompound();
+                    type.setTag("name", new NBTString("minecraft:overworld"));
+                    type.setTag("id", new NBTInt(0));
+                    type.setTag("element", dimension);
+                    types.addTag(type);
+                    dimensionType.setTag("type", new NBTString("minecraft:dimension_type"));
+                    dimensionType.setTag("value", types);
+                    dimensionCodec.setTag("minecraft:dimension_type", dimensionType);
+
+                    NBTCompound biomeRegistry = new NBTCompound();
+                    biomeRegistry.setTag("type", new NBTString("minecraft:worldgen/biome"));
+                    NBTList<NBTCompound> biomes = new NBTList<>(NBTType.COMPOUND);
+                    NBTCompound biomeRegEntry = new NBTCompound();
+                    biomeRegEntry.setTag("name", new NBTString("minecraft:ocean"));
+                    biomeRegEntry.setTag("id", new NBTInt(0));
+                    NBTCompound oceanElement = new NBTCompound();
+                    oceanElement.setTag("precipitation", new NBTString("none"));
+                    oceanElement.setTag("depth", new NBTFloat(1.6f));
+                    oceanElement.setTag("temperature", new NBTFloat(1.0f));
+                     oceanElement.setTag("scale", new NBTFloat(1.0f));
+                     oceanElement.setTag("downfall", new NBTFloat(0.6f));
+                     oceanElement.setTag("category", new NBTString("ocean"));
+                     NBTCompound oceanEffects = new NBTCompound();
+                     oceanEffects.setTag("sky_color", new NBTInt(8364543));
+                    oceanEffects.setTag("water_fog_color", new NBTInt(8364543));
+                    oceanEffects.setTag("fog_color", new NBTInt(8364543));
+                     oceanEffects.setTag("water_color", new NBTInt(4159204));
+                     oceanElement.setTag("effects", oceanEffects);
+                    biomeRegEntry.setTag("element", oceanElement);
+                    biomes.addTag(biomeRegEntry);
+                    biomeRegistry.setTag("value", biomes);
+                    dimensionCodec.setTag("minecraft:worldgen/biome", biomeRegistry);
+
+                    WrapperPlayServerJoinGame joinGame = new WrapperPlayServerJoinGame(user.getEntityId(),
+                            false, user.getGameMode(), user.getPreviousGameMode(),
+                            worldNames, dimensionCodec, dimension, worldNames.get(0), hashedSeed, Graphene.MAX_PLAYERS, 10, 20,
+                            true, true, false, true);
+
+                    PacketEvents.getAPI().getPlayerManager().sendPacket(event.getChannel(), joinGame);
+
+                    WrapperPlayServerHeldItemChange heldItemChange = new WrapperPlayServerHeldItemChange(0);
+                    PacketEvents.getAPI().getPlayerManager().sendPacket(event.getChannel(), heldItemChange);
+
+                    System.out.println("send held item change!");
+
+                    int flags = 0x01;
+                    flags |= 0x02;
+                    flags |= 0x04;
+                    flags |= 0x08;
+                    flags |= 0x10;
+                    WrapperPlayServerPlayerPositionAndLook positionAndLook = new WrapperPlayServerPlayerPositionAndLook(0, 0, 0, 0.0f, 0.0f, flags, 0);
+                    PacketEvents.getAPI().getPlayerManager().sendPacket(event.getChannel(), positionAndLook);
+                    System.out.println("send position and look!");
                 }
                 break;
             case PLAY:
@@ -98,6 +191,10 @@ public class InternalPacketListener implements PacketListener {
                     tabCompleteAttribute.setInput(text);
                     Optional<Integer> transactionID = tabComplete.getTransactionId();
                     transactionID.ifPresent(tabComplete::setTransactionId);
+                }
+                else if (event.getPacketType() == PacketType.Play.Client.CLIENT_SETTINGS) {
+                    WrapperPlayClientSettings settings = new WrapperPlayClientSettings(event);
+                    System.out.println("got settings, hand: " + settings.getHand());
                 }
                 break;
         }
--- src/main/java/com/github/graphene/user/User.java
+++ src/main/java/com/github/graphene/user/User.java
@@ -2,16 +2,21 @@ package com.github.graphene.user;
 
 import com.github.retrooper.packetevents.protocol.ConnectionState;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
+import com.github.retrooper.packetevents.protocol.player.GameMode;
 import io.netty.channel.Channel;
 
 import java.util.UUID;
 
 public class User {
+    public static int ENTITY_COUNT = 0;
     private final Channel channel;
     private String username;
     private UUID uuid;
     private ConnectionState state;
     private ClientVersion clientVersion;
+    private final int entityID = ENTITY_COUNT++;
+    private GameMode gameMode = GameMode.SURVIVAL;
+    private GameMode previousGameMode = null;
 
     public User(Channel channel, ConnectionState state) {
         this.channel = channel;
@@ -52,6 +57,26 @@ public class User {
 
     public void setClientVersion(ClientVersion clientVersion) {
         this.clientVersion = clientVersion;
+    }
+
+    public int getEntityId() {
+        return entityID;
+    }
+
+    public GameMode getGameMode() {
+        return gameMode;
+    }
+
+    public void setGameMode(GameMode gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    public GameMode getPreviousGameMode() {
+        return previousGameMode;
+    }
+
+    public void setPreviousGameMode(GameMode previousGameMode) {
+        this.previousGameMode = previousGameMode;
     }
 
     public void forceDisconnect() {
